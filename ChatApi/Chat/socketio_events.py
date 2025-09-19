4c927f9e71a99a1e5d62d0e0cbe4e8a2f2411371
biruktadele <biruktadele55@gmail.com>
2025-09-19 17:41:27 +0300

    Updated

--- ChatApi/Chat/socketio_events.py
+++ ChatApi/Chat/socketio_events.py
@@ -44,8 +44,8 @@ async def save_and_broadcast_message(chat_id, user_id, content):
         message_payload = {
             'id': msg.id,
             'chat_id': msg.chat_id.id,
-            'sender': msg.sender_id,
-            'username': msg.sender_id.username,
+            'sender_id': msg.sender_id.id,
+            'sender_username': msg.sender_id.username,
             'content': msg.content,
             'created_at': msg.created_at.isoformat(),
             'is_read': msg.is_read,
