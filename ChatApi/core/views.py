a8428d76116ad7c8cffc726a27f61799a2cb227d
Biruk Tadele <106767562+Biruktadele@users.noreply.github.com>
2025-10-24 22:47:21 +0300

    Refactor UserViewSet actions for better clarity
    Refactor user actions in UserViewSet to use dedicated methods for GET, PUT, PATCH, and DELETE requests. Update the method names for clarity and maintain RESTful conventions.

--- ChatApi/core/views.py
+++ ChatApi/core/views.py
@@ -1,29 +1,41 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import User
-from .serializers import *
+from .serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    @action(detail=False, methods=['get' , 'put'])
-    def me(self, request):
-        user = User.objects.get(id=request.user.id)
+    @action(detail=False, methods=['get'], url_path='me')
+    def get_me(self, request):
+        user = request.user
+        serializer = self.get_serializer(user)
+        return Response(serializer.data)
 
-        if request.method == 'GET':
-            serializer = UserSerializer(user)
-            return Response(serializer.data)
+    @get_me.mapping.put
+    def update_me(self, request):
+        user = request.user
+        serializer = self.get_serializer(user, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
-        if request.method == 'PUT':
-            serializer = UserSerializer(user, data=request.data)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data)
+    @get_me.mapping.patch
+    def partial_update_me(self, request):
+        user = request.user
+        serializer = self.get_serializer(user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
-# accounts/views.py
+    @get_me.mapping.delete
+    def delete_me(self, request):
+        user = request.user
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 from rest_framework import generics
 from .serializers import RegisterSerializer
 # from django.contrib.auth.models import User
